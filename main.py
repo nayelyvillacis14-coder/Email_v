fe44b597d1aa085c29a47c3af8bf1919d1edcd39
nayelyvillacis14-coder <nayelyvillacis4@gmail.com>
2026-02-02 00:09:08 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from src.src.persona import PersonaServicio
-
-app = QApplication(sys.argv)
-vtn_principal = PersonaServicio()
-vtn_principal.show()
-sys.exit(app.exec())
